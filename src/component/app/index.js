f3f2e729cdc79ef983f688870376a529d5bf1f34
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-12-09 14:07:19 -0800

    basic form working

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import NoteForm from '../note-form';
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      notes: [],
+    };
+  }
+
+  componentDidUpdate(){
+    console.log('__STATE__', this.state);
+  }
+
   render(){
     return (
-      <h1> Sup Betches </h1>
+      <div>
+        <h1> Sup Betches </h1>
+        <NoteForm />
+      </div>
     )
   }
 }
---
+++ src/component/note-form/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import uuid from 'uuid';
+
+class NoteForm extends React.Component {
+  constructor(props){
+    super(props);
+  
+    this.state = {
+      note: {
+        id: uuid(),
+        editing: false,
+        completed: false,
+        content: '',
+      }
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e){
+    this.setState({content: e.target.value});
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    console.log(this.state);
+  }
+
+
+  render(){
+    return(
+      <form className = 'note-form' onSubmit = {this.handleSubmit}>
+        <input
+          onChange = {this.handleChange}
+          placeholder = 'Enter Note Here'
+          type = 'text'
+          name = 'content'
+        />
+        <button type='submit'> Create Note </button>
+      </form>
+    )
+  }
+}
+
+
+export default NoteForm;
