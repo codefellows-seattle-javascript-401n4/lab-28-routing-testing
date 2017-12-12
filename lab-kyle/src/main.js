60f54998ec4ea9954e4734deeaf04a9c609a2757
Kyle Richey <richey.kyle.i@gmail.com>
2017-12-11 20:47:43 -0800

    finishing up some basic setup

---
+++ lab-kyle/src/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state ={
+
+    }
+  }
+  componentDidUpdate(){
+    console.log('__STATE__', this.state);
+  }
+  render(){
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+ReactDom.render(<App />, document.getElementById('root'));
