d418f9bdb71828c5d9526fba839b9756ee0eba77
Rowen Scout <rowen.scout@gmail.com>
2018-01-26 14:12:53 -0800

    restarting, too many errors.

--- rowen-lab/src/main.js
+++ rowen-lab/src/main.js
@@ -1,49 +1,26 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import {BrowserRouter,Route} from 'react-router-dom';
+'use strict';
+console.log('this works!');
 
-class App extends React.Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            notes:[],
-            showErrors:true
-        }
-        //maintaining context
-        this.app = this.app.bind(this);
-        // this.function = this.function.bind(this);
-
-    }
-
-    componentDidMount() {
-        console.log("__STATE__", this.state);
-    }
-
-    updateTheNote(note) {
-        this.setState({note})
-    }
-
-    app() {
-        return {
-            state: this.state,
-            setState: this.setState.bind(this),
-            showErrors: true
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <Header appTitle="React Note App" />
-                <main>
-                    <Route exact path=/ component={Home} />
-                    <Route exact path=/notes component={() => <Expenses handler={this.updateTheNote} app={this.app()}/>} />
-                </main>
-            </div>
-        )
-    }
-}
-
-ReactDom.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById(root));
+// import './style/main.scss';
+//
+// import React from 'react';
+// import ReactDom from 'react-dom';
+// import {BrowserRouter,Route} from 'react-router-dom';
+//
+// class App extends React.Component {
+//
+//     constructor(props) {
+//         super(props);
+//     }
+//
+//     render() {
+//         return (
+//             <div className="note-home">
+//                 <NoteCreateForm />
+//                 <NoteList />
+//             </div>
+//         )
+//     }
+// }
+//
+// ReactDom.render(<App/>, document.getElementById('root'));
